fdeac81e4f25e9a2188eb050023ed10f168d68c8
dhughen93 <dhughe9373@students.pcci.edu>
2015-02-17 13:55:59 -0600

    modified by Andrea: simple-sidebar.css, teacherClassPage.html, by David: login.php, teacherClassPage.php, teacherMainPage.php

--- login.php
+++ login.php
@@ -48,37 +48,35 @@ $stmt->execute();
 $type = ""; // Determines whether a student or teacher or nothing
 
 // Fetch all of the results one at a time. Access them from the variables in the bind_result
+
+
 while($stmt->fetch())
 {
    echo $stu1 . " - " . $stu2 . " - ";
 }
 
+$tea->execute();
+while($tea->fetch())
+{
+   echo $tea1 . " - " . $tea2 . " - ";
+}
 // Post student id and go to student main page
 if($stu1 != null)
 {
 	$_SESSION['username'] = $id;
 	header('Location: studentMainPage.php');
 }
-$stmt->close();
-
-$tea->execute();
-
-while($tea->fetch())
-{
-	echo $tea1 . " - " . $tea2 . " - ";
-}
-
 // Post teacher id and go to teacher main page
-if($tea1 != null)
+else if($tea1 != null)
 {
 	$_SESSION['username'] = $id;
 	header('Location: teacherMainPage.php');
 }
+else
+{
+	 header('Location: login.html');
 
-   // Go back to login and say incorrect (ADD CODE FOR MESSSAGE DOWN HERE!!)
-   echo "<script language=javascript>
-	     javascript:history.back();		
-        </script>";
+}
 
 // Close the database connection
 mysqli_close($database);
