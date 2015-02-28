90aef401c054ce21ca4cee94fa9c69509946b3f9
[wizardbab] <[fastswimmer4saipan@gmail.com]>
2015-02-28 15:53:19 -0600

    Email and password checking for pattern code

--- signUp.php
+++ signUp.php
@@ -3,22 +3,22 @@
 
 <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta name="description" content="">
+   <meta name="author" content="">
 
-    <title>Test Republic</title>
+   <title>Test Republic</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+   <!-- Bootstrap Core CSS -->
+   <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="css/simple-sidebar.css" rel="stylesheet">
-	
-	   <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+   <!-- Custom CSS -->
+   <link href="css/simple-sidebar.css" rel="stylesheet">
+
+   <!-- Custom Fonts -->
+   <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
 </head>
 
@@ -43,106 +43,120 @@ if (mysqli_connect_errno())
 $listClassQuery = "select class_description from class";
 
 
-
-
-
 @ $database->select_db(DATABASENAME);
 
-
-// Check to see if anything was entered, if not assign and empty string
+// Check to see if anything was entered, if not assign an empty string
 $firstName = (isset($_POST['firstName']) ? $_POST['firstName'] : "");
-$lastName  = (isset($_POST['lastName']) ? $_POST['lastName'] : "");
-$email     = (isset($_POST['email']) ? $_POST['email'] : "");
-$password  = (isset($_POST['password']) ? $_POST['password'] : "");
+$lastName  = (isset($_POST['lastName'])  ? $_POST['lastName'] : "");
+$email     = (isset($_POST['email'])     ? $_POST['email'] : "");
+$password  = (isset($_POST['password'])  ? $_POST['password'] : "");
 
 ?>
-		
 
+<div id="sidebar-wrapper">
+   <a href="logout.php">
+      <!-- Button with a link wrapped around it to go back to the login page -->
+      <button class="btn btn-block btn-primary" type="button" id="signUpButton" onclick="signUp.php">
+         <i class="glyphicon glyphicon-log-in"></i>Back to Login
+      </button>
+   </a>
+   <ul class="sidebar-nav">
+      <li>
+         <a href="#" id="student-summary">Summary</a>
+      </li>
+      <li class="sidebar-brand"><!-- VIC AND ANDREA, I'D LIKE FOR THIS TO "SELECT A CLASS TO ADD:" (formatting needed) -->
+         Select a Class:
+      </li>
+      <?php 
+         $courseCounter = 1;
+         // List of classes for the user to select from...we'll need to keep track of what class is selected for the db
+         if ($classList = $database->prepare($listClassQuery)) 
+         {
+            // nothing to bind
+         }
+         else
+         {
+            printf("Error message: %s\n", $database->error);
+         }
+         $classList->bind_result($clde);
+         $classList->execute();
+         while($classList->fetch())
+         {
+            echo '<li><a href="#"><div class="subject-name">' . $courseCounter++ . ". " . $clde . '</div></a></li>';
+         }
+         $classList->close(); 
+      ?>
+   </ul>
+</div>
 
-	
-					<div id="sidebar-wrapper">
-					
-					<a href="logout.php">
-					<!-- Button with a link wrapped around it to go back to the login page -->
-					<button class="btn btn-block btn-primary" type="button" id="signUpButton" onclick="signUp.php">
-						<i class="glyphicon glyphicon-log-in"></i>Back to Login
-					</button></a>
-            <ul class="sidebar-nav">
-				<li>
-                    <a href="#" id="student-summary">Summary</a>
-                </li>
-                <li class="sidebar-brand"><!-- VIC AND ANDREA, I'D LIKE FOR THIS TO "SELECT A CLASS TO ADD:" (formatting needed) -->
-                    Select a Class:
-                </li>
-               
-				<?php 
-				$courseCounter = 1;
-				// List of classes for the user to select from...we'll need to keep track of what class is selected for the db
-				if ($classList = $database->prepare($listClassQuery)) 
-				{
-					// nothing to bind
-				}
-				else {
-					printf("Errormessage: %s\n", $database->error);
-				}	
-				$classList->bind_result($clde);
-				$classList->execute();
-				while($classList->fetch())
-				{	
-					echo '<li><a href="#"><div class="subject-name">' . $courseCounter++ . ". " . $clde . '</div></a></li>';
-				}
-				$classList->close(); 
-				?>
-            </ul>
-				
-				
-        </div>
-		  
 <form name="signUpForm" id="signUpForm" action="signUp.php" method="post">
-	<div id="signUpDiv">
-		<h1>Welcome!</h1>
-			<h2>Please enter your information.</h2>
-			<label class="survey_style">First Name:
-				<input type="text" name="firstName" id="firstName" />
-			</label><br />
-			<label class="survey_style">Last Name:
-				<input type="text" name="lastName" id="lastName" />
-			</label><br />
-			<label class="survey_style">Email:
-				<input type="text" name="email" id="email" />
-			</label><br />
-			<label class="survey_style">Password:
-				<input type="text" name="password" id="password" />
-			</label><br />
-			<input class="myButton" type="submit" value="Create Account" />
-			
-	
-			
-		<table class="signUpTable">
-					<tr><td><?php echo $firstName ?></td></tr>
-					<tr><td><?php echo $lastName ?></td></tr>
-					<tr><td><?php echo $email ?></td></tr>
-					<tr><td><?php echo $password ?></td></tr>
-					
-		</table>
-	</div>
+   <div id="signUpDiv">
+      <h1>Welcome!</h1>
+         <h2>Please enter your information.</h2>
+         <label class="survey_style">First Name:
+            <input type="text" name="firstName" id="firstName" />
+         </label><br />
+         <label class="survey_style">Last Name:
+            <input type="text" name="lastName" id="lastName" />
+         </label><br />
+         <label class="survey_style">Email:
+            <input type="text" name="email" id="email" />
+         </label><br />
+         <label class="survey_style">Password:
+            <input type="text" name="password" id="password" />
+         </label><br />
+      <input class="myButton" type="submit" value="Create Account" />
+
+      <table class="signUpTable">
+         <tr><td><?php echo $firstName ?></td></tr>
+         <tr><td><?php echo $lastName ?></td></tr>
+         <tr><td>
+         <?php
+            // Does a preliminary check for email pattern
+            if(!preg_match('^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+^', $email))
+            {
+               echo "Invalid email ".$email;
+            }
+            else
+            {
+               echo "Valid email ".$email;
+            }
+         ?>
+         </td></tr>
+         <tr><td>
+         <?php
+            // Does a preliminary check for required password pattern
+            if(!preg_match('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*_).+^',$password))
+            {
+               echo "Not enough types of characters: ";
+            }
+            else
+            {
+               if(!preg_match('^.{8,16}^', $password))
+                  echo "Not enough characters: ";
+               else
+                  echo "Enough characters: ";
+            }
+            echo $password;
+         ?>
+         </td></tr>
+      </table>
+   </div>
 </form>
 
+   <!-- jQuery -->
+   <script src="js/jquery.js"></script>
 
+   <!-- Bootstrap Core JavaScript -->
+   <script src="js/bootstrap.min.js"></script>
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Menu Toggle Script -->
-    <script>
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-    </script>
+   <!-- Menu Toggle Script -->
+   <script>
+   $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+   });
+   </script>
 
 </body>
 
