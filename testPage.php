a8af001b1ad545120f86efa73bf9437a55aa6d67
andreasetiawan <asetia7281@students.pcci.edu>
2015-03-23 21:16:48 -0500

    new test taking page, All that apply not totally working yet

--- testPage.php
+++ testPage.php
@@ -182,61 +182,207 @@ $_SESSION['testId'] = $testId;
 					echo '<br />';
 				} */
 				?>	
-    <div id="wrapper">
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-				<li class="sidebar-brand">
-                    Question Types:
-                </li>
-				<li>
-					<div class="radio">
-						<label><input type="radio" id="essayRadio" name="optradio">Essay</label>
-					</div>
-				</li>
-				<li>
-					<div class="radio">
-						<label><input type="radio" name="optradio">Fill in the Blank</label>
-					</div>
-				</li>
-				<li>
-					<div class="radio">
-						<label><input type="radio" name="optradio">T/F</label>
-					</div>
-				</li>
-				<li>
-					<div class="radio disabled">
-						<label><input type="radio" name="optradio" >Multiple Choice</label>
-					</div>
-				</li>
-				<li>
-					<div class="radio">
-						<label><input type="radio" name="optradio">Matching</label>
-					</div>
-				</li>
-				<li>
-					<div class="radio">
-						<label><input type="radio" name="optradio">Short Answer</label>
-					</div>
-				</li>
-				<li>
-					<div class="radio">
-						<label><input type="radio" name="optradio">All that Apply</label>
-					</div>
-				</li>
-				<li>
-					<div class="radio">
-						<label><input type="radio" id="summaryRadio" name="optradio">Summary</label>
-					</div>
-				</li>
-				<li>
-					<div id="test">
-					</div>
-				</li>
-            </ul>
+   
+   
+	 <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">CS 306
+                    <small>Test 1</small>
+                </h1>
+            </div>
         </div>
-		  
-	<script>
+        <!-- /.row -->
+
+        <!-- Content Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel-group" id="accordion">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Multiple Choice Questions</a>
+                            </h4>
+                        </div>
+                        <div id="collapseOne" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                <h3>Question 1: <small>1+1=?</small><h3>
+								<input type="radio" name="mc_answer1" id="mc_answer1" value="multipleRadio1" class="multipleRadio">11</input>
+								<input type="radio" name="mc_answer1" id="mc_answer1" value="multipleRadio1" class="multipleRadio">2</input>
+								<input type="radio" name="mc_answer1" id="mc_answer1" value="multipleRadio1" class="multipleRadio">14</input>
+								<input type="radio" name="mc_answer1" id="mc_answer1" value="multipleRadio1" class="multipleRadio">87</input>
+							</div>
+							<div class="panel-body">
+								<h3>Question 2: <small>Andrea is ...</small><h3>
+								<input type="radio" name="mc_answer2" id="mc_answer2" value="multipleRadio2" class="multipleRadio">Indo</input>
+								<input type="radio" name="mc_answer2" id="mc_answer2" value="multipleRadio2" class="multipleRadio">Viet</input>
+								<input type="radio" name="mc_answer2" id="mc_answer2" value="multipleRadio2" class="multipleRadio">An Alien</input>
+								<input type="radio" name="mc_answer2" id="mc_answer2" value="multipleRadio2" class="multipleRadio">Short</input>
+							</div>
+							<div class="panel-body">
+								<h3>Question 3: <small>Cats are awesome</small><h3>
+								<input type="radio" name="mc_answer3" id="mc_answer3" value="multipleRadio3" class="multipleRadio">True</input>
+								<input type="radio" name="mc_answer3" id="mc_answer3" value="multipleRadio3" class="multipleRadio">rreally true</input>
+								<input type="radio" name="mc_answer3" id="mc_answer3" value="multipleRadio3" class="multipleRadio">very true</input>
+								<input type="radio" name="mc_answer3" id="mc_answer3" value="multipleRadio3" class="multipleRadio">MEGA TRUE</input>
+							</div>
+                        </div>
+                    </div>
+                    <!-- /.panel -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Matching Choice Questions</a>
+                            </h4>
+                        </div>
+                        <div id="collapseTwo" class="panel-collapse collapse">
+                            <div class="panel-body container">
+                                <h3>Question 1: <small>Math Problems!</small><h3>
+								<div class="col-md-6">
+									<input type="text" name="mc_answer1" id="mc_answer1"class="multipleRadio">1+1=</input>
+									<input type="text" name="mc_answer1" id="mc_answer1"class="multipleRadio">2+2=</input>
+								</div>
+								<div class="col-md-6">
+									<h1> a. 2</h1>
+									<h1> b. 2</h1>
+									<h1> c. EPIC</h1>
+									<h1> d. 2</h1>
+								</div>
+							</div>
+                        </div>
+                    </div>
+                    <!-- /.panel -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">All That Apply Questions</a>
+                            </h4>
+                        </div>
+                        <div id="collapseThree" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                <h3>Question 1: <small>Jake is...</small><h3>
+								<input type="checkbox" name="ata_answer1" id="ata_answer_cb1" class="ata_cb">Ugly</input>
+								<input type="checkbox" name="ata_answer1" id="ata_answer_cb1" class="ata_cb">a DB God</input>
+								<input type="checkbox" name="ata_answer1" id="ata_answer_cb1" class="ata_cb">Fat</input>
+								<input type="checkbox" name="ata_answer1" id="ata_answer_cb1" class="ata_cb">Assistant Project manager</input>
+							</div>
+							<div class="panel-body">
+								<h3>Question 2: <small>Blue is...</small><h3>
+								<input type="checkbox" name="ata_answer2" id="ata_answer_cb2" class="ata_cb">Ugly</input>
+								<input type="checkbox" name="ata_answer2" id="ata_answer_cb2" class="ata_cb">our color</input>
+								<input type="checkbox" name="ata_answer2" id="ata_answer_cb2" class="ata_cb">meow</input>
+								<input type="checkbox" name="ata_answer2" id="ata_answer_cb2" class="ata_cb">Ugly</input>
+							</div>
+							<div class="panel-body">
+								<h3>Question 3: <small>Death comes to...</small><h3>
+								<input type="checkbox" name="ata_answer3" id="ata_answer_cb3" class="ata_cb">Infidels</input>
+								<input type="checkbox" name="ata_answer3" id="ata_answer_cb3" class="ata_cb">Ugly people</input>
+								<input type="checkbox" name="ata_answer3" id="ata_answer_cb3" class="ata_cb">Pretty People</input>
+								<input type="checkbox" name="ata_answer3" id="ata_answer_cb3" class="ata_cb">Sick people</input>
+							</div>
+                        </div>
+                    </div>
+                    <!-- /.panel -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">True/False Questions</a>
+                            </h4>
+                        </div>
+                        <div id="collapseFour" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                <h3>Question 1: <small>David is ugly</small><h3>
+								<input type="radio" name="tf_answer1" id="tf_answer1" value="multipleRadio1" class="multipleRadio">True</input>
+								<input type="radio" name="tf_answer1" id="tf_answer1" value="multipleRadio1" class="multipleRadio">False</input>
+							</div>
+							<div class="panel-body">
+								<h3>Question 2: <small>Dr. Howell is a boss.</small><h3>
+								<input type="radio" name="tf_answer2" id="tf_answer2" value="multipleRadio2" class="multipleRadio">True</input>
+								<input type="radio" name="tf_answer2" id="tf_answer2" value="multipleRadio2" class="multipleRadio">False</input>
+							</div>
+							<div class="panel-body">
+								<h3>Question 3: <small>Cats are awesome</small><h3>
+								<input type="radio" name="tf_answer3" id="tf_answer3" value="multipleRadio3" class="multipleRadio">True</input>
+								<input type="radio" name="tf_answer3" id="tf_answer3" value="multipleRadio3" class="multipleRadio">False</input>
+							</div>
+                        </div>
+                    </div>
+                    <!-- /.panel -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseFive">Short Answer</a>
+                            </h4>
+                        </div>
+                        <div id="collapseFive" class="panel-collapse collapse">
+							<div class="panel-body">
+								<h2>Question 1: <small>E=MC?</small><h2>
+								<input type="text" class="m_answer_letter form-control" id="ShortAnswer1" />
+							</div>
+							<div class="panel-body">
+								<h2>Question 2: <small>What is the meaning of life</small><h2>
+								<input type="text" class="m_answer_letter form-control" id="ShortAnswer2" />
+							</div>
+							<div class="panel-body">
+								<h2>Question 3: <small>Who is the main hero in Ranger's Apprentice?</small><h2>
+								<input type="text" class="m_answer_letter form-control" id="ShortAnswer3" />
+							</div>
+                        </div>
+                    </div>
+                    <!-- /.panel -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseSix">Essay Questions</a>
+                            </h4>
+                        </div>
+                        <div id="collapseSix" class="panel-collapse collapse">
+                            <div class="panel-body">
+								<h2>Question 1: <small>Why Should Victor get a raise?</small><h2>
+								<textarea class="form-control" id="EssayQuestion1" name="specificInstruction" rows="6"> </textarea>
+							</div>
+							<div class="panel-body">
+								<h2>Question 2: <small>Who is your teacher?</small><h2>
+								<textarea class="form-control" id="EssayQuestion2" name="specificInstruction" rows="6"> </textarea>
+						   </div>
+							<div class="panel-body">
+								<h2>Question 3: <small>Which team rocks your socks off?</small><h2>
+								<textarea class="form-control" id="EssayQuestion3" name="specificInstruction" rows="6"> </textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.panel-group -->
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+		<div class="row">
+			<h4 id="pledgeHeader"> Do you accept the terms of agreement? </h4>
+		</div>
+		
+		<div class="row">
+			<div class="col-md-3">
+				<input type="text" class="form-control" id="Pledge">
+			</div>
+			
+			<div class="col-md-9">	
+				<button type="button" class="btn btn-primary btn-block" id="Submit">Submit</button>
+			</div>
+		</div>
+		
+		
+		
+   </div>
+   <!-- /. Container -->
+   
+	
+
+    
+		<script>
 	$(document).ready(function()
 	{
 		$("#summaryRadio").click(function()
@@ -268,143 +414,6 @@ $_SESSION['testId'] = $testId;
 		});
 	});
 	</script>
-	
-		
-        <!-- /#sidebar-wrapper -->
-		
-		<div class="row" id="test_name">
-			Test #1
-		</div>
-		
-		<div class="row">
-			<div class="col-lg-12" id="question_header">
-				<span id="question_text">Question</span>
-				<a href="#"><span class="glyphicon glyphicon-chevron-left"></span></a>
-				<input type="text" name="question_number" id="question_number" value="1" />
-				<a href="#"><span class="glyphicon glyphicon-chevron-right"></span></a>
-			</div>
-		</div>
-		
-		
-        <!-- Page Content -->
-        <div id="page-content-wrapper">
-		<!-- Keep page stuff under this div! -->
-            <div class="container-fluid">
-				<div class="row">
-					<div class="col-lg-12" id="question_type_text">
-						<!-- don't touch this -->
-						Essay
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-lg-12" id="QuestionBox">
-						<h3 id="question" onload="loadFirstQuestion()"> </h3>
-						<div class="form-group">
-						  <textarea class="form-control" rows="10" id="AnswerBox"></textarea>
-						</div>	
-					</div>
-					<button type="button" class="btn btn-default" id="prevBtn"><span class="glyphicon glyphicon-chevron-left"></span>previous</button>
-					<button type="button" class="btn btn-default" id="nxtBtn"><span class="glyphicon glyphicon-chevron-right"></span>next</button>
-				</div>
-				
-				<!-- Essay template 
-				<div class="row">
-					<div class="col-lg-12">
-						<h1> Essay Question: </h1>
-						<h3> What is the meaning of life?</h3>
-						<div class="form-group">
-						  <textarea class="form-control" rows="10" id="comment"></textarea>
-						</div>
-					<ul class="pager">
-						<li class="previous"> <a href="#"><span class="glyphicon glyphicon-chevron-left"></span> previous </a></li>
-						<li class="next"> <a href="#"><span class="glyphicon glyphicon-chevron-right"></span> next </a></li>
-					</ul>
-					</div>
-				</div> -->
-				
-				
-				<!-- /Essay template -->
-				
-				<!-- Multiple Choice template 
-				<div class="row">
-					<div class="col-lg-12">
-						<h1> Multiple Choice: </h1>
-						<h3> Which is not true? </h3>
-						<div class="form-group">
-							<form role="form">
-								<div class="checkbox">
-								  <label><input type="checkbox" value="">true</label>
-								</div>
-								<div class="checkbox">
-								  <label><input type="checkbox" value="">false</label>
-								</div>
-								<div class="checkbox">
-								  <label><input type="checkbox" value="">true</label>
-								</div>
-							</form>
-						</div>
-					<ul class="pager">
-						<li class="previous"> <a href="#"><span class="glyphicon glyphicon-chevron-left"></span> previous </a></li>
-						<li class="next"> <a href="#"><span class="glyphicon glyphicon-chevron-right"></span> next </a></li>
-					</ul>
-					</div>
-				</div>
-				 /Multiple Choice Template -->
-				
-				<!-- Short Answer 
-				<div class="row">
-                    <div class="col-lg-12">
-						<h1> Short Answer: </h1>
-						<h3> I am feeling... </h3>
-                    </div>
-                </div>
-				<div class="row">
-					<div class="col-lg-12">
-						<div class="form-group">
-						  <textarea class="form-control" rows="1" id="comment"></textarea>
-						</div>
-					<ul class="pager">
-						<li class="previous"> <a href="#"><span class="glyphicon glyphicon-chevron-left"></span> previous </a></li>
-						<li class="next"> <a href="#"><span class="glyphicon glyphicon-chevron-right"></span> next </a></li>
-					</ul>
-					</div>
-				</div> -->
-				
-				
-				<!-- /short Answer -->
-				
-				<!-- T/F template 
-				<div class="row">
-					<div class="col-lg-12">
-						<h1> True/False: </h1>
-						<h3> The Mongolian Horde is a beast team. </h3>
-						<div class="form-group">
-							<form role="form">
-								<div class="radio">
-									<label><input type="radio" name="optradio">True</label>
-								</div>
-								<div class="radio">
-									<label><input type="radio" name="optradio">False</label>
-								</div>
-							</form>
-						</div>
-					<ul class="pager">
-						<li class="previous"> <a href="#"><span class="glyphicon glyphicon-chevron-left"></span> previous </a></li>
-						<li class="next"> <a href="#"><span class="glyphicon glyphicon-chevron-right"></span> next </a></li>
-					</ul>
-					</div>
-				</div>
-				 /T/F Template -->
-				
-				
-            </div>
-        </div>
-        <!-- /#page-content-wrapper -->
-    </div>
-    <!-- /#wrapper -->
-	
-
-    
 	
     <!-- Menu Toggle Script -->
     <script>
